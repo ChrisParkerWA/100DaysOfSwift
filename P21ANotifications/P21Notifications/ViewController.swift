81e1894653c309ee88b59db08cf0d6fa12bbd4a5
Chris Parker <boxerflat4@iinet.net.au>
2019-10-17 14:18:36 +0800

    Added Xcode project P21ANotifications

---
+++ P21ANotifications/P21Notifications/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  P21Notifications
+//
+//  Created by Chris Parker on 6/5/19.
+//  Copyright © 2019 Chris Parker. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+    }
+    
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
+    }
+
+    @objc func scheduleLocal() {
+        
+        registerCategories()
+        
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        //  Challenge 1: Update the code in didReceive so that it shows different instances of
+        //  UIAlertController depending on which action identifer was passed in.
+        
+        let action1 = UNNotificationAction(identifier: "action1", title: "Select first action", options: .foreground)
+        
+        let action2 = UNNotificationAction(identifier: "action2", title: "Select second action", options: .foreground)
+        
+        let category = UNNotificationCategory(identifier: "alarm", actions: [action1, action2], intentIdentifiers: [], options: [])
+        
+        center.setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // pull out the buried userInfo dictionary
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+            
+            switch response.actionIdentifier {
+                
+            case UNNotificationDefaultActionIdentifier:
+                // the user swiped to unlock
+                print("Default Identifier")
+                
+            //  Challenge 1: Update the code in didReceive so that it shows different instances of
+            //  UIAlertController depending on which action identifer was passed in.
+            case "action1":
+               showAlert("Action 1", message: "The first action was selected")
+                
+            case "action2":
+               showAlert("Action 2", message: "The second action was selected")
+                
+            default:
+                break
+            }
+        }
+        
+        // you must call the completion handler when you're done
+        completionHandler()
+    }
+    
+    func showAlert(_ title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+    
+}
+
