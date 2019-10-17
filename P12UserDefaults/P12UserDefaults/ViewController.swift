5fecb650d7733f38e22a068ce0d38e25f4038d42
Chris Parker <boxerflat4@iinet.net.au>
2019-10-17 14:08:10 +0800

    Added Xcode project P12UserDefaults

---
+++ P12UserDefaults/P12UserDefaults/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  P12UserDefaults
+//
+//  Created by Chris Parker on 23/3/19.
+//  Copyright © 2019 Chris Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        
+        defaults.set("Chris Parker", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Chris", "Country": "UK"]
+        defaults.set(dict, forKey: "SavedDictionary")
+        
+        let savedInteger = defaults.integer(forKey: "Age")
+        let savedBoolean = defaults.bool(forKey: "UserFaceID")
+        
+        let savedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        let savedDictionary = defaults.object(forKey: "SavedDictionary") as? [String: String] ?? [String: String]()
+        
+    }
+
+
+}
+
