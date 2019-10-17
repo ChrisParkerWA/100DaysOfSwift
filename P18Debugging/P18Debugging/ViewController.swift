b899d628b137b92a886b683044ccdc8782f9802a
Chris Parker <boxerflat4@iinet.net.au>
2019-10-17 14:14:46 +0800

    Added Xcode project P18Debugging

---
+++ P18Debugging/P18Debugging/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  P18Debugging
+//
+//  Created by Chris Parker on 16/4/19.
+//  Copyright © 2019 Chris Parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
